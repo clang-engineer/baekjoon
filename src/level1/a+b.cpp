ca732a9cfb2427f160d5ac71a98ed8e639adfed0
yorez <yorez@naver.com>
2022-08-12 11:31:42 +0900

    feat: a+p.cpp

---
+++ src/level1/a+b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int  a, b;
+
+	std::cout << "input two number: " << std::endl;
+	std::cin >> a;
+	std::cin >> b;
+
+	std::cout << a + b << std::endl;
+
+    return 0;
+}
+

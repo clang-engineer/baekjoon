bf78ca37ad2731f6069078c9530a018b02b40cd1
yorez <yorez@naver.com>
2022-08-12 12:24:26 +0000

    fix: typo

--- src/level1/remainder.cpp
+++ src/level1/remainder.cpp
@@ -13,7 +13,7 @@ int main()
 
 	cout << (A + B) % C << endl;
 	cout << ((A % C) + (B % C)) % C << endl;
-	cout << (A * C) % C << endl;
+	cout << (A * B) % C << endl;
 	cout << ((A % C) * (B % C)) % C << endl;
 
     return 0;

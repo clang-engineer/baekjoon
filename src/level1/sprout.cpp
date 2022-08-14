9fb413b777ad46c752c02f162e9a55e40b97b6e0
yorez <yorez@naver.com>
2022-08-14 12:51:46 +0000

    feat: sproud cpp

--- src/level1/sprout.cpp
+++ src/level1/sprout.cpp
@@ -8,7 +8,7 @@ int main()
 
 	cout << "         ,r'\"7" << endl;
 	cout << "r`-_   ,'  ,/" << endl;
-	cout << " \. \". L_r'" << endl;
+	cout << " \\. \". L_r'" << endl;
 	cout << "   `~\\/" << endl;
 	cout << "      |" << endl;
 	cout << "      |" << endl;

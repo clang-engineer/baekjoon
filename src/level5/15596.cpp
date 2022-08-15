9bf5924a59087a30ad8c57ce73b21815ea3a169f
yorez <yorez@naver.com>
2022-08-16 08:42:30 +0900

    fix: 15596

--- src/level5/15596.cpp
+++ src/level5/15596.cpp
@@ -1,35 +1,9 @@
-#include <iostream>
 #include <vector>
 
-int sum_of_vec(std::vector<int>& vec);
-
-int main()
-{
-	int input_num;
-	std::cin >> input_num;
-
-	std::vector<int> arr;
-
-	for (int i = 0; i < input_num; i++)
-	{
-		int temp;
-		std::cin >> temp;
-		arr.push_back(temp);
+long long sum(std::vector<int> &a) {
+	long long res = 0;
+	for (int i = 0; i < a.size(); i++) {
+		res += a[i];
 	}
-
-	int sum = sum_of_vec(arr);
-	std::cout << sum << std::endl;
-
-    return 0;
+	return res;
 }
-
-int sum_of_vec(std::vector<int> &vec)
-{
-	int sum = 0;
-	for (auto iter = vec.begin(); iter != vec.end(); ++iter)
-	{
-		sum += *iter;
-	}
-	return sum;
-}
-

2c224301c4b83101d44dc729558bdd7bf6ea8164
yorez <yorez@naver.com>
2022-08-18 17:12:51 +0900

    fix: 1152 - trim logic

--- src/level6/1152.cpp
+++ src/level6/1152.cpp
@@ -4,6 +4,9 @@
 
 
 std::vector<std::string> split(std::string input, char delimiter);
+void trim(std::string &s);
+void ltrim(std::string &s);
+void rtrim(std::string &s);
 
 int main()
 {
@@ -11,6 +14,7 @@ int main()
 
 	int count = 0;
 	std::getline(std::cin, target_str);
+	trim(target_str);
 
 	std::vector<std::string> result = split(target_str, ' ');
 
@@ -33,4 +37,24 @@ std::vector<std::string> split(std::string input, char delimiter) {
 	}
 
 	return vec;
+}
+
+// trim from start (in place)
+void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+}
+
+// trim from end (in place)
+void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+// trim from both ends (in place)
+void trim(std::string &s) {
+    ltrim(s);
+    rtrim(s);
 }

d73f5176d2b1d4c57cfed8348d49ad1354e62a2b
yorez <yorez@naver.com>
2022-08-18 16:57:03 +0900

    feat: 1152

---
+++ src/level6/1152.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+
+std::vector<std::string> split(std::string input, char delimiter);
+
+int main()
+{
+	std::string target_str;
+	std::vector<std::string> vec;
+
+	int count = 0;
+	std::getline(std::cin, target_str);
+
+	std::vector<std::string> result = split(target_str, ' ');
+
+	for (int i=0; i < result.size(); i++){
+		count++;
+	}
+
+	std::cout << count << std::endl;
+	return 0;
+}
+
+std::vector<std::string> split(std::string input, char delimiter) {
+	std::vector<std::string> answer;
+	std::stringstream ss(input);
+	std::string temp;
+
+	while (getline(ss, temp, delimiter)) {
+		answer.push_back(temp);
+	}
+
+	return answer;
+}

640f2629508ad3b9b2ef785777c8ff68783b7bb4
yorez <yorez@naver.com>
2022-08-18 17:21:06 +0900

    fix: 1152 - compile err

--- src/level6/1152.cpp
+++ src/level6/1152.cpp
@@ -18,7 +18,8 @@ int main()
 
 	std::vector<std::string> result = split(target_str, ' ');
 
-	for (int i=0; i < result.size(); i++){
+	for (auto iter = result.begin(); iter != result.end(); ++iter)
+	{
 		count++;
 	}
 
@@ -39,22 +40,19 @@ std::vector<std::string> split(std::string input, char delimiter) {
 	return vec;
 }
 
-// trim from start (in place)
 void ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-        return !std::isspace(ch);
-    }));
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+				return !std::isspace(ch);
+				}));
 }
 
-// trim from end (in place)
 void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-        return !std::isspace(ch);
-    }).base(), s.end());
+	s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+				return !std::isspace(ch);
+				}).base(), s.end());
 }
 
-// trim from both ends (in place)
 void trim(std::string &s) {
-    ltrim(s);
-    rtrim(s);
+	ltrim(s);
+	rtrim(s);
 }

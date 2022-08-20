75ea609994762fa139832ee487c13a98425356a7
yorez <yorez@naver.com>
2022-08-20 14:23:57 +0000

    fix: 1712

--- src/level7/1712.cpp
+++ src/level7/1712.cpp
@@ -8,21 +8,17 @@ int main()
 	std::cin >> B;
 	std::cin >> C;
 
-	if (A <= 0 || B <= 0 || C <= 0)
+	if ((C - B) <= 0)
 	{
 		std::cout << -1 << std::endl; 
 		return 0;
 	}
-	
+
 	int result = A / (C - B) + 1;
 
 	if (result > 0)
 	{
 		std::cout << result << std::endl; 
-	}
-	else
-	{
-		std::cout << -1 << std::endl; 
 	}
 
 	return 0;

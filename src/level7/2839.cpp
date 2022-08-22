b903c454bf3be2ce6f4522e331d681089b9506c1
yorez <yorez@naver.com>
2022-08-22 16:34:28 +0900

    feat: 2839

--- src/level7/2839.cpp
+++ src/level7/2839.cpp
@@ -13,19 +13,19 @@ int main()
 	}
 	else if ((target % 5) % 3 == 0)
 	{
-		std::cout << target / 5 + target / 5 / 3 << std::endl;
+		std::cout << target / 5 + (target % 5) / 3 << std::endl;
 	}
-	else if ((target % 5) == 1)
+	else if ((target % 5) == 1 & target >= 6)
 	{
-		std::cout << target / 5 + (target / 5 + 5) / 3 - 1 << std::endl;
+		std::cout << target / 5 + (target % 5 + 5) / 3 - 1 << std::endl;
 	}
-	else if ((target % 5) == 2)
+	else if ((target % 5) == 2 & target >= 12)
 	{
-		std::cout << target / 5 + (target / 5 + 10) / 3 - 2 << std::endl;
+		std::cout << target / 5 + (target % 5 + 10) / 3 - 2 << std::endl;
 	}
-	else if ((target % 5) == 4)
+	else if ((target % 5) == 4 & target >= 24)
 	{
-		std::cout << target / 5 + (target / 5 + 20) / 3 - 4 << std::endl;
+		std::cout << target / 5 + (target % 5 + 20) / 3 - 4 << std::endl;
 	}
 	else if (target % 3 == 0)
 	{

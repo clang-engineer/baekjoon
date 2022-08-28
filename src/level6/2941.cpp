f4975174141c28c23ab3392c1b6a57451c70b730
yorez <yorez@naver.com>
2022-08-28 13:30:59 +0000

    feat: 2941

--- src/level6/2941.cpp
+++ src/level6/2941.cpp
@@ -13,6 +13,10 @@ int main()
 
 	for (int i = 0; i < input.length() - 2; i++)
 	{
+		if (input.length() < 3)
+		{
+			break;
+		}
 		std::string target_string = input.substr(i, 3);
 		if (std::find(vec.begin(), vec.end(), target_string) != vec.end())
 		{
@@ -21,8 +25,13 @@ int main()
 		}
 	}
 
+
 	for (int i = 0; i < input.length() - 1; i++)
 	{
+		if (input.length() < 2)
+		{
+			break;
+		}
 		std::string target_string = input.substr(i, 2);
 		if (std::find(vec.begin(), vec.end(), target_string) != vec.end())
 		{

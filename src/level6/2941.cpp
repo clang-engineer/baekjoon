925359f8ff61df90e05be866e240b24231c40e74
yorez <yorez@naver.com>
2022-08-28 13:13:07 +0000

    feat: 2941

--- src/level6/2941.cpp
+++ src/level6/2941.cpp
@@ -11,34 +11,30 @@ int main()
 
 	int count = 0;
 
-	std::string replaced1 = input;
-
 	for (int i = 0; i < input.length() - 2; i++)
 	{
-		std::string target_string = replaced1.substr(i, 3);
+		std::string target_string = input.substr(i, 3);
 		if (std::find(vec.begin(), vec.end(), target_string) != vec.end())
 		{
 			count++;
-			replaced1.replace(replaced1.find(target_string), (target_string).size(), "0");
+			input.replace(input.find(target_string), (target_string).length(), "0");
 		}
 	}
 
-	std::string replaced2 = replaced1;
-
-	for (int i = 0; i < replaced1.length() - 1; i++)
+	for (int i = 0; i < input.length() - 1; i++)
 	{
-		std::string target_string = replaced2.substr(i, 2);
+		std::string target_string = input.substr(i, 2);
 		if (std::find(vec.begin(), vec.end(), target_string) != vec.end())
 		{
 			count++;
-			replaced2.replace(replaced2.find(target_string), (target_string).size(), "0");
+			input.replace(input.find(target_string), (target_string).length(), "0");
 		}
 
 	}
 
-	for (int i = 0; i < replaced2.size(); i++)
+	for (int i = 0; i < input.length(); i++)
 	{
-		if (replaced2[i] != '0')
+		if (input[i] != '0')
 		{
 			count++;
 		}

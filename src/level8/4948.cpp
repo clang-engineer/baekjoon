182dcb4de37a255695261f0c4d682790b54d4982
yorez <yorez@naver.com>
2022-09-01 08:59:13 +0900

    fix: 4948 - bertrand

--- src/level8/4948.cpp
+++ src/level8/4948.cpp
@@ -20,33 +20,23 @@ int main()
 		int max_num = 2 * num;
 		bool* prime_arr = new bool[max_num + 1];
 
-		for (int i = 2; i <= max_num; i++)
+		for (int i = 2; i <= num; i++)
 		{
-			prime_arr[i] = true;
-		    if (i * 2 <= max_num) prime_arr[i * 2] = false;
-		    if (i * 3 <= max_num) prime_arr[i * 3] = false;
-		    if (i * 4 <= max_num) prime_arr[i * 4] = false;
-		    if (i * 5 <= max_num) prime_arr[i * 5] = false;
-		    if (i * 6 <= max_num) prime_arr[i * 6] = false;
-		    if (i * 7 <= max_num) prime_arr[i * 7] = false;
-		    if (i * 11 <= max_num) prime_arr[i * 11] = false;
-		    if (i * 13 <= max_num) prime_arr[i * 13] = false;
-		    if (i * 17 <= max_num) prime_arr[i * 17] = false;
-		    if (i * 19 <= max_num) prime_arr[i * 19] = false;
-		    if (i * 23 <= max_num) prime_arr[i * 23] = false;
+			if (is_prime(i))
+			{
+				for (int j = i * 2; j <= max_num; j += i)
+				{
+					prime_arr[j] = true;
+				}
+			}
 		}
 
 		for (int i = num + 1; i <= max_num; i++)
 		{
-			if (!prime_arr[i]) {
-				continue;
-			} else {
-				if (is_prime(i))
-				{
-					count += 1;
-					continue;
-				}
-			} 
+			if(!prime_arr[i])
+			{
+				count++;
+			}
 		}
 
 		std::cout << count << "\n";
@@ -56,30 +46,13 @@ int main()
 
 bool is_prime(int num)
 {
-	if (num <= 1)
-	{
-		return false;
-	}
-
-	if (num == 2 || num == 3 || num == 5 || num == 7)
-	{
-		return true;
-	}
-
-	if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0)
-	{
-		return false;
-	}
-
-	bool is_prime = true;
 	for (int i = 2; i <= std::sqrt(num); i++)
 	{
 		if (num % i == 0)
 		{
-			is_prime = false;
-			break;
+			return false;
 		}
 	}
 
-	return is_prime;
+	return true;
 }

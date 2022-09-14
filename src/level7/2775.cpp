700b8246a70c2c2892e00520803100724bcc3159
yorez <yorez@naver.com>
2022-09-14 16:22:46 +0900

    feat: 2750 cpp

---
+++ src/level7/2775.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int k, n;
+
+	std::cin >> k >> n;
+
+	int sum = 0;
+
+	for (int i = 0; i < k; i++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+		}
+	}
+    return 0;
+}
+

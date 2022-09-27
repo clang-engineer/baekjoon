6ad4b669051a730823b843a48eea4eea7a6f4649
yorez <yorez@naver.com>
2022-09-28 08:45:08 +0900

    feat: 10814

--- src/level9/10814.cpp
+++ src/level9/10814.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <algorithm>
 
 struct compare {
 	bool operator()(const std::pair<int, std::string> &a, const std::pair<int, std::string> &b) {
 		if (a.first == b.first) {
-			return true;
+			return false;
 		}
-		return a.first > b.first;
+		return a.first < b.first;
 	}
 };
 
@@ -16,24 +16,24 @@ int main()
 	int total_count;
 	std::cin >> total_count;
 
-	std::priority_queue<std::pair<int, std::string>, std::vector<std::pair<int,std::string>>, compare> pq;
+	std::vector<std::pair<int, std::string>> vec;
 
 	while (total_count > 0) {
 		int age;
 		std::string name;
 		std::cin >> age >> name;
 
-		pq.push(std::make_pair(age, name));
+		vec.push_back(std::make_pair(age, name));
 
 		total_count--;
 	}
 
 
-	while (!pq.empty()) {
-		std::cout << pq.top().first << " " << pq.top().second << std::endl;
-		pq.pop();
+	std::stable_sort(vec.begin(), vec.end(), compare());
+
+	for (auto &v : vec) {
+		std::cout << v.first << " " << v.second << "\n";
 	}
 
 	return 0;
 }
-

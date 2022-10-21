5af9a56e546f9e095fe2784ef9c3a9224b01dd6e
yorez <yorez@naver.com>
2022-10-21 09:17:52 +0900

    fix: 2108

--- src/level9/2108.cpp
+++ src/level9/2108.cpp
@@ -3,6 +3,7 @@
 #include <numeric>
 #include <algorithm>
 #include <map>
+#include <cmath>
 
 int get_median(std::vector<int> &v) {
 	std::sort(v.begin(), v.end());
@@ -10,7 +11,10 @@ int get_median(std::vector<int> &v) {
 }
 
 int get_average(std::vector<int> &v) {
-	return std::round(std::accumulate(v.begin(), v.end(), 0) / v.size());
+	int parent = std::accumulate(v.begin(), v.end(), 0);
+	int child = v.size();
+
+	return std::round((double)parent / child);
 }
 
 int get_range(std::vector<int> &v) {
@@ -39,7 +43,19 @@ int get_max_mode(std::map<int, int> &m) {
 	return max;
 }
 
-int get_count_in_map_by_value(std::map<int, int> &m, int value) {
+int get_max_mode_elem(std::map<int, int> &m) {
+	int result;
+	int max = 0;
+	for (auto &i : m) {
+		if (i.second > max) {
+			max = i.second;
+			result = i.first;
+		}
+	}
+	return result;
+}
+
+int get_max_node_count_in_map_by_value(std::map<int, int> &m, int value) {
 	int count = 0;
 	for (auto &i : m) {
 		if (i.second == value) {
@@ -49,9 +65,19 @@ int get_count_in_map_by_value(std::map<int, int> &m, int value) {
 	return count;
 }
 
-int get_second_smallest_in_vector(std::vector<int> &v) {
-	std::sort(v.begin(), v.end());
-	return v[1];
+int get_second_smallest_in_mode(std::map<int, int> &m) {
+	std::vector<int> temp;
+
+	for (auto &i : m) {
+		if (i.second == get_max_mode(m)) {
+			temp.push_back(i.first);
+		}
+	}
+
+	std::sort(temp.begin(), temp.end());
+
+
+	return temp[1];
 }
 
 int main()
@@ -69,20 +95,20 @@ int main()
 		total_num--;
 	}
 
-	std::cout << "===" << std::endl;
 	std::cout << get_average(vec) << std::endl;
 	std::cout << get_median(vec) << std::endl;
 	std::map<int, int> mode = get_mode(vec);
+
 	if (vec.size() == 1) {
 		std::cout << vec[0] << std::endl;
-	}
-	else if (get_count_in_map_by_value(mode, get_max_mode(mode)) > 1) {
-		std::cout << get_second_smallest_in_vector(vec) << std::endl;
+	} 
+	else if (get_max_node_count_in_map_by_value(mode, get_max_mode(mode)) == 1) {
+		std::cout << get_max_mode_elem(mode) << std::endl;
 	} else {
-		std::cout << get_max_mode(mode) << std::endl;
+		std::cout << get_second_smallest_in_mode(mode) << std::endl;
 	}
 
 	std::cout << get_range(vec) << std::endl;
 
-    return 0;
+	return 0;
 }

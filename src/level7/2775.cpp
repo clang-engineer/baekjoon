c2895ba855854283989e7629468239c56e6170b1
yorez <yorez@naver.com>
2022-12-21 09:06:53 +0900

    feat: 2775

--- src/level7/2775.cpp
+++ src/level7/2775.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
 #include <vector>
 
+class Solution {
+	private: 
+		int getMemberOfRoom(int k, int n) {
+			if (k == 0) {
+				return n;
+			}
+			int sum = 0;
+			for (int i = 1; i <= n; i++) {
+				sum += getMemberOfRoom(k - 1, i);
+			}
+			return sum;
+		}
+	public:
+		int answer(int k, int n) {
+			return getMemberOfRoom(k, n);
+		}
+
+};
+
 int main()
 {
-	int k, n;
+	int T;
 
-	std::cin >> k >> n;
+	std::cin >> T;
 
-	int sum = 0;
-
-	for (int i = 0; i < k; i++)
-	{
-		for (int i = 0; i < n; i++)
-		{
-		}
+	while (T--) {
+		int k, n;
+		std::cin >> k >> n;
+		Solution s;
+		std::cout << s.answer(k, n) << std::endl;
 	}
+
     return 0;
 }
 

1e859f3695a6b6943e95a2189b6075bba08b1cb7
hhkbdev <yorez@naver.com>
2023-07-09 11:15:00 +0900

    feat: cmake for excutable app

---
+++ app/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+#include "shape.h"
+
+class Solution {
+ public:
+  void answer() {}
+};
+
+int main() {
+  shape::Rectangle r(2, 3);
+  r.Print();
+  return 0;
+}
+

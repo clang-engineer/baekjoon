160db1c7344f3f15024d80f836d022018f96d456
hhkbdev <yorez@naver.com>
2023-07-17 21:30:06 +0900

    feat: 19532

--- app/main.cpp
+++ app/main.cpp
@@ -1,40 +1,14 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
+
+#include "_19532.h"
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-
-    int N;
-
-    std::cin >> N;
-
-    std::unordered_map<std::string, std::string> input;
-
-    for (int i = 0; i < N; i++) {
-        std::string name, status;
-        std::cin >> name >> status;
-
-        if (status == "enter") {
-            input[name] = status;
-        } else {
-            input.erase(name);
-        }
-    }
+    int a, b, c, d, e, f;
+    std::cin >> a >> b >> c >> d >> e >> f;
+    _19532::Solution solution(a, b, c, d, e, f);
+    std::cout << solution.GetX() << " " << solution.GetY() << std::endl;
+    return 0;
 
 
-    std::vector<std::string> result;
-    for (const auto &i: input) {
-        result.push_back(i.first);
-    }
-
-    std::sort(result.begin(), result.end(), std::greater<std::string>());
-
-    for (const auto &i: result) {
-        std::cout << i << "\n";
-    }
 }
 
---
+++ include/_19532.h
@@ -0,0 +1,18 @@
+#ifndef BAEKJOON_INCLUDE__19532_H_
+#define BAEKJOON_INCLUDE__19532_H_
+
+
+namespace _19532 {
+class Solution {
+  private :
+    int a, b, c, d, e, f;
+public:
+    Solution(int a, int b, int c, int d, int e, int f);
+    int GetX();
+    int GetY();
+};
+
+};
+
+
+#endif

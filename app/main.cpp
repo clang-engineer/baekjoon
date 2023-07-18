ebb63be76a7cad6b3fd3b1d75ffa8122aad7c002
hhkbdev <yorez@naver.com>
2023-07-18 09:34:44 +0900

    feat: 1620

--- app/main.cpp
+++ app/main.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
 
-#include "_19532.h"
+#include "_1620.h"
 
 int main() {
-    int a, b, c, d, e, f;
-    std::cin >> a >> b >> c >> d >> e >> f;
-    _19532::Solution solution(a, b, c, d, e, f);
-    std::cout << solution.GetX() << " " << solution.GetY() << std::endl;
-    return 0;
+    int N, M;
 
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
 
+    std::cin >> N >> M;
+
+    std::unordered_map<std::string, int> m;
+    std::vector<std::string> v;
+
+    for (int i = 0; i < N; i++) {
+        std::string s;
+        std::cin >> s;
+        m[s] = i;
+        v.push_back(s);
+    }
+
+    _1620::Solution solution(m, v);
+
+    for (int i = 0; i < M; i++) {
+        std::string s;
+        std::cin >> s;
+        std::cout << solution.FindMatch(s) << '\n';
+    }
 }
 

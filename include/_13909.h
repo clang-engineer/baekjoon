2ac184a331c0f09dd562ab9c3d20120356b4b1d0
hhkbdev <yorez@naver.com>
2023-07-19 09:06:30 +0900

    feat: _13909

---
+++ include/_13909.h
@@ -0,0 +1,18 @@
+#ifndef BAEKJOON__13909_H
+#define BAEKJOON__13909_H
+
+
+namespace _13909 {
+    class Solution {
+    private:
+        int N;
+    public:
+        Solution(int N_);
+
+        int GetOpenMirrorNumber();
+    };
+
+};
+
+
+#endif

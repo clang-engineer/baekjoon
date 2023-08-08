20517f94e13422655c589df47cffda1674bb1518
hhkbdev <yorez@naver.com>
2023-08-09 08:40:29 +0900

    feat: 2164

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <queue>
-#include <string>
 
 int main() {
   std::ios::sync_with_stdio(false);
@@ -13,42 +12,24 @@ int main() {
 
   std::queue<int> q;
 
-  while (N > 0) {
-    std::string cmd;
-
-    std::cin >> cmd;
-
-    if (cmd == "push") {
-      int num;
-      std::cin >> num;
-      q.push(num);
-    } else if (cmd == "pop") {
-      if (q.empty()) {
-        std::cout << -1 << '\n';
-      } else {
-        std::cout << q.front() << '\n';
-        q.pop();
-      }
-    } else if (cmd == "size") {
-      std::cout << q.size() << '\n';
-    } else if (cmd == "empty") {
-      std::cout << q.empty() << '\n';
-    } else if (cmd == "front") {
-      if (q.empty()) {
-        std::cout << -1 << '\n';
-      } else {
-        std::cout << q.front() << '\n';
-      }
-    } else if (cmd == "back") {
-      if (q.empty()) {
-        std::cout << -1 << '\n';
-      } else {
-        std::cout << q.back() << '\n';
-      }
-    }
-
-    N--;
+  for (int i = 1; i <= N; i++) {
+    q.push(i);
   }
+
+  int index = 1;
+
+  while (q.size() > 1) {
+    if (index % 2 == 1) {
+      q.pop();
+    } else {
+      int temp = q.front();
+      q.pop();
+      q.push(temp);
+    }
+    index++;
+  }
+
+  std::cout << q.front() << '\n';
 
   return 0;
 }

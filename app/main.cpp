dd7778ad919479bb039522ee2ec54df08d93f8f9
hhkbdev <yorez@naver.com>
2023-08-09 09:03:40 +0900

    feat: 28279

--- app/main.cpp
+++ app/main.cpp
@@ -1,38 +1,72 @@
+#include <deque>
 #include <iostream>
-#include <queue>
 
 int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
 
-  int N, K;
+  int N;
 
-  std::cin >> N >> K;
+  std::cin >> N;
 
-  std::queue<int> q;
+  std::deque<int> dq;
 
-  for (int i = 1; i <= N; i++) {
-    q.push(i);
-  }
-
-  std::cout << "<";
-
-  int index = 0;
-
-  while (!q.empty()) {
-    index++;
-    if (q.size() == 1) {
-      std::cout << q.front() << ">";
-      std::cout << "\n";
+  for (int i = 0; i < N; i++) {
+    int cmd;
+    std::cin >> cmd;
+    int x;
+    switch (cmd) {
+    case 1:
+      std::cin >> x;
+      dq.push_front(x);
       break;
-    } else if (index == K) {
-      std::cout << q.front() << ", ";
-      q.pop();
-      index = 0;
-    } else {
-      q.push(q.front());
-      q.pop();
+    case 2:
+      std::cin >> x;
+      dq.push_back(x);
+      break;
+    case 3:
+      if (dq.empty()) {
+        std::cout << -1 << '\n';
+      } else {
+        std::cout << dq.front() << '\n';
+        dq.pop_front();
+      }
+      break;
+    case 4:
+      if (dq.empty()) {
+        std::cout << -1 << '\n';
+      } else {
+        std::cout << dq.back() << '\n';
+        dq.pop_back();
+      }
+      break;
+    case 5:
+      std::cout << dq.size() << '\n';
+      break;
+    case 6:
+      if (dq.empty()) {
+        std::cout << 1 << '\n';
+      } else {
+        std::cout << 0 << '\n';
+      }
+      break;
+    case 7:
+      if (dq.empty()) {
+        std::cout << -1 << '\n';
+      } else {
+        std::cout << dq.front() << '\n';
+      }
+      break;
+    case 8:
+      if (dq.empty()) {
+        std::cout << -1 << '\n';
+      } else {
+        std::cout << dq.back() << '\n';
+      }
+      break;
+    default:
+      break;
     }
   }
 

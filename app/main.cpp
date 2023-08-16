bdbceb77adce609fa02a2264e6a41c164a898381
hhkbdev <yorez@naver.com>
2023-08-16 09:39:55 +0900

    feat: 24511

--- app/main.cpp
+++ app/main.cpp
@@ -1,29 +1,47 @@
+#include <deque>
 #include <iostream>
 #include <vector>
-#include "_1037.h"
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
 
-    std::vector<long long> vec;
+  int N;
+  std::cin >> N;
 
-    int N;
-    std::cin >> N;
+  std::vector<int> structure(N);
 
-    for (int i = 0; i < N; i++) {
-        long long num;
-        std::cin >> num;
-        vec.push_back(num);
+  for (int i = 0; i < N; ++i) {
+    std::cin >> structure[i];
+  }
+
+  std::deque<int> dq;
+
+  for (int i = 0; i < N; ++i) {
+    int num;
+    std::cin >> num;
+
+    if (structure[i] == 0) {
+      dq.push_front(num);
     }
+  }
 
+  int M;
+  std::cin >> M;
 
-    _1037::Solution solution(vec);
-    std::cout << solution.solution() << '\n';
+  for (int i = 0; i < M; ++i) {
+    int num;
+    std::cin >> num;
 
+    dq.push_back(num);
 
+    std::cout << dq.front() << ' ';
+    dq.pop_front();
+  }
 
-    return 0;
+  std::cout << '\n';
+
+  return 0;
 }
 

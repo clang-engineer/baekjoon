02a68c8a3b1e189a67e2c37a6b2c9320e0e84f45
hhkbdev <yorez@naver.com>
2023-08-23 08:59:21 +0900

    feat: 25501

--- app/main.cpp
+++ app/main.cpp
@@ -1,52 +1,33 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <string>
+#include <cstring>
+
+
+std::string recursion(const char *s, int l, int r,  int count = 0){
+    if(l >= r) return "1 " + std::to_string(count);
+    else if(s[l] != s[r]) return "0 " + std::to_string(count);
+    else return recursion(s, l+1, r-1, count+1);
+}
+
+std::string isPalindrome(const char *s){
+    return recursion(s, 0, strlen(s)-1, 1);
+}
+
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
 
-    int N, M;
+    int N;
     std::cin >> N;
 
-    std::vector<int> vec1(N);
+    for (int i = 0; i < N; i++) {
+        std::string s;
+        std::cin >> s;
 
-    for (int i = 0; i < N; ++i) {
-        std::cin >> vec1[i];
+        std::cout << isPalindrome(s.c_str()) << "\n";
     }
-
-    std::sort(vec1.begin(), vec1.end());
-
-    std::cin >> M;
-
-    std::vector<int> vec2(M);
-
-    for (int i = 0; i < M; ++i) {
-        std::cin >> vec2[i];
-    }
-
-    for (int i = 0; i < M; ++i) {
-        int left = 0;
-        int right = N - 1;
-        int mid = 0;
-        bool found = false;
-        while (left <= right) {
-            mid = (left + right) / 2;
-            if (vec1[mid] == vec2[i]) {
-                found = true;
-                break;
-            } else if (vec1[mid] < vec2[i]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        std::cout << found << '\n';
-    }
-
-
-
     return 0;
 }
 

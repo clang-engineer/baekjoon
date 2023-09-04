4439236aa903cd13d8fae0ea980db699156fde26
hhkbdev <yorez@naver.com>
2023-09-04 09:16:31 +0900

    feat: 15649

--- app/main.cpp
+++ app/main.cpp
@@ -1,33 +1,43 @@
 #include <iostream>
-#include <string>
-#include <cstring>
+#define MAX 9
 
+int N, M;
 
-std::string recursion(const char *s, int l, int r,  int count = 0){
-    if(l >= r) return "1 " + std::to_string(count);
-    else if(s[l] != s[r]) return "0 " + std::to_string(count);
-    else return recursion(s, l+1, r-1, count+1);
+int arr[MAX] = {
+    0,
+};
+
+bool visited[MAX] = {
+    0,
+};
+
+void dfs(int cnt) {
+  if (cnt == M) {
+    for (int i = 0; i < M; i++) {
+      std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+    return;
+  }
+
+  for (int i = 1; i <= N; i++) {
+    if (!visited[i]) {
+      visited[i] = true;
+      arr[cnt] = i;
+      dfs(cnt + 1);
+      visited[i] = false;
+    }
+  }
 }
-
-std::string isPalindrome(const char *s){
-    return recursion(s, 0, strlen(s)-1, 1);
-}
-
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
 
-    int N;
-    std::cin >> N;
+  std::cin >> N >> M;
+  dfs(0);
 
-    for (int i = 0; i < N; i++) {
-        std::string s;
-        std::cin >> s;
-
-        std::cout << isPalindrome(s.c_str()) << "\n";
-    }
-    return 0;
+  return 0;
 }
 

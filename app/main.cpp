efb7758901037eb03bc40f0248e5454aaedfeb71
hhkbdev <yorez@naver.com>
2023-09-08 09:48:10 +0900

    feat: 1463

--- app/main.cpp
+++ app/main.cpp
@@ -1,34 +1,16 @@
 #include <iostream>
-#define MAX 9
+#include "_1463.h"
 
-int N, M;
-
-int arr[MAX] = {
-    0,
-};
-
-void dfs(int num, int cnt) {
-  if (cnt == M) {
-    for (int i = 0; i < M; i++) {
-      std::cout << arr[i] << " ";
-    }
-    std::cout << "\n";
-    return;
-  }
-
-  for (int i = num; i <= N; i++) {
-    arr[cnt] = i;
-    dfs(i, cnt + 1);
-  }
-}
 
 int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
 
-  std::cin >> N >> M;
-  dfs(1, 0);
+  int N;
+  std::cin >> N;
+
+    std::cout << _1463::GetMake1OperationCount(N) << std::endl;
 
   return 0;
 }
---
+++ include/_1463.h
@@ -0,0 +1,3 @@
+namespace _1463 {
+int GetMake1OperationCount(int num);
+} // namespace _1463

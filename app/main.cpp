1f15f77889ba2ca66df038e21f7b6573fd81d167
hhkbdev <yorez@naver.com>
2023-09-11 09:20:59 +0900

    feat: 29736

--- app/main.cpp
+++ app/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include "_1463.h"
+#include "_29736.h"
 
 
 int main() {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-  std::cout.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
 
-  int N;
-  std::cin >> N;
+    int lower, upper, stand, gap;
+    std::cin >> lower >> upper >> stand >> gap;
 
-    std::cout << _1463::GetMake1OperationCount(N) << std::endl;
-
-  return 0;
+    std::cout << _29736::GetPossibleCount(lower, upper, stand, gap) << std::endl;
+    return 0;
 }
 
---
+++ include/_29736.h
@@ -0,0 +1,5 @@
+#include <string>
+
+namespace _29736 {
+    std::string GetPossibleCount(int lower, int uppper, int stand, int gap);
+}

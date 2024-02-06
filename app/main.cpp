b0f05386dcd9569354c264a8d02dab22f574293f
clang-engineer <clang.engineer@gmail.com>
2024-02-06 09:08:16 +0900

    feat: 1325

--- app/main.cpp
+++ app/main.cpp
@@ -1,43 +1,29 @@
 #include <iostream>
-#include "_18352.h"
-#include <vector>
-#include <algorithm>
+#include "_1325.h"
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
 
-    long N, M, K, X;
+    long N, M;
+    std::cin >> N >> M;
 
-    std::cin >> N >> M >> K >> X;
-
-    std::vector<std::pair<int, int>> edges(M);
+    _1325::N = N;
+    _1325::A.resize(N+1);
+    _1325::answer.resize(N+1);
+    _1325::visited.resize(N+1);
 
     for (int i = 0; i < M; i++) {
-        std::cin >> edges[i].first >> edges[i].second;
+        int s, e;
+        std::cin >> s >> e;
+        _1325::A[s].push_back(e);
     }
 
+    std::vector<int> result = _1325::GetResult();
 
-    _18352::Solution solution(N, edges);
-
-    std::vector<int> result = solution.getBFSDistance(X);
-
-    std::vector<int> answer;
-
-    for (int i = 0; i < N; i++) {
-        if (result[i] == K) {
-            answer.push_back(i + 1);
-        }
-    }
-
-    if (!answer.empty()) {
-        std::sort(answer.begin(), answer.end());
-        for (int i = 0; i < answer.size(); i++) {
-            std::cout << answer[i] << "\n";
-        }
-    } else if (answer.empty()) {
-        std::cout << -1 << "\n";
+    for (int i: result) {
+        std::cout << i << " ";
     }
 
     return 0;

b13c08755cddd5acecfaaf8f33c512eff8c661f9
clang-engineer <clang.engineer@gmail.com>
2024-02-08 10:11:32 +0900

    feat: 3412 origin

--- app/main.cpp
+++ app/main.cpp
@@ -1,74 +1,49 @@
 #include <iostream>
 #include <vector>
-
-#include "_1043.h"
+#include <queue>
 
 int main() {
     std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
+    std::cin.tie(0);
+    std::cout.tie(0);
 
-    int N, M, T;
-    std::cin >> N >> M >> T;
+    typedef std::vector<int> vi;
+    int N, M;
+    std::cin >> N >> M;
 
-    // init true parent
-    _1043::trueP.resize(T);
+    std::vector<vi> A;
+    vi indegree;
 
-    for (int i = 0; i < T; i++) {
-        std::cin >> _1043::trueP[i];
-    }
-
-
-    // init party member
-    _1043::party.resize(M);
+    A.resize(N + 1);
+    indegree.resize(N + 1);
 
     for (int i = 0; i < M; i++) {
-        int party_size;
-        std::cin >> party_size;
+        int S, E;
+        std::cin >> S >> E;
+        A[S].push_back(E);
+        indegree[E]++;
+    }
 
-        for (int j = 0; j < party_size; j++) {
-            int temp;
-            std::cin >> temp;
-            _1043::party[i].push_back(temp);
+    std::queue<int> queue;
+
+    for (int i = 1; i <= N; i++) {
+        if (indegree[i] == 0) {
+            queue.push(i);
         }
     }
 
-
-    // init origin parent
-    _1043::parent.resize(N + 1);
-
-    for (int i = 0; i <= N; i++) {
-        _1043::parent[i] = i;
-    }
-
-
-    // union party
-    for (int i = 0; i < M; i++) {
-        int firstPeople = _1043::party[i][0];
-        for (int j = 1; j < _1043::party[i].size(); j++) {
-            _1043::unionfunc(firstPeople, _1043::party[i][j]);
-        }
-    }
-
-
-    // check is possible
-    for (int i = 0; i < M; i++) {
-        bool isPossible = true;
-        int cur = _1043::party[i][0];
-
-        for (int j = 0; j < T; j++) {
-            if (_1043::find(cur) == _1043::find(_1043::trueP[j])) {
-                isPossible = false;
-                break;
+    while (!queue.empty()) {
+        int front = queue.front();
+        queue.pop();
+        std::cout << front << " ";
+        for (int i = 0; i < A[front].size(); i++) {
+            int next = A[front][i];
+            indegree[next]--;
+            if (indegree[next] == 0) {
+                queue.push(next);
             }
         }
-
-        if (isPossible) {
-            _1043::result++;
-        }
     }
 
-    std::cout << _1043::result;
+
 }
-
-

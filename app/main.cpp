e9554af804da54994d0af719c77725c8b5158edf
clang-engineer <clang.engineer@gmail.com>
2024-02-10 11:19:00 +0900

    feat: 1753 test

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <limits.h>
 
-typedef std::pair<int, int> edge;
-
-std::vector<int> GetDijkstraDistance(int node_count, int start, std::vector<std::vector<edge>> &graph);
+#include "_1753.h"
 
 int main() {
     std::ios::sync_with_stdio(false);
@@ -14,7 +11,7 @@ int main() {
 
     int node_count, edge_count, start_node;
 
-    std::vector<std::vector<edge>> graph;
+    std::vector<std::vector<_1753::edge>> graph;
 
     std::cin >> node_count >> edge_count >> start_node;
 
@@ -26,7 +23,7 @@ int main() {
         graph[u].push_back({v, w});
     }
 
-    std::vector<int> result = GetDijkstraDistance(node_count, start_node, graph);
+    std::vector<int> result = _1753::GetDijkstraDistance(node_count, start_node, graph);
 
     for (int i = 1; i <= node_count; i++) {
         if (result[i] == INT_MAX) {
@@ -38,41 +35,3 @@ int main() {
 
 }
 
-std::vector<int> GetDijkstraDistance(
-        int node_count,
-        int start_node,
-        std::vector<std::vector<edge>> &graph) {
-    std::priority_queue<edge, std::vector<edge>, std::greater<edge>> pq;
-
-    std::vector<int> distance;
-    distance.resize(node_count + 1);
-    std::fill(distance.begin(), distance.end(), INT_MAX);
-
-    std::vector<bool> visited;
-    visited.resize(node_count + 1);
-    std::fill(visited.begin(), visited.end(), false);
-
-    pq.push(std::make_pair(0, start_node));
-    distance[start_node] = 0;
-
-    while (!pq.empty()) {
-        edge current = pq.top();
-        pq.pop();
-
-        int current_node = current.second;
-        if (visited[current_node]) continue;
-
-        visited[current_node] = true;
-
-        for (auto &next: graph[current_node]) {
-            int next_node = next.first;
-            int next_weight = next.second;
-
-            if (distance[next_node] > distance[current_node] + next_weight) {
-                distance[next_node] = distance[current_node] + next_weight;
-                pq.push(std::make_pair(distance[next_node], next_node));
-            }
-        }
-    }
-    return distance;
-}

e3488718c0ded2280a4043dd32507f3e288d7be3
clang-engineer <clang.engineer@gmail.com>
2024-02-12 13:16:36 +0900

    refactor: floyd warshall - split lib & add case

--- app/main.cpp
+++ app/main.cpp
@@ -1,40 +1,7 @@
 #include <iostream>
 #include <vector>
 
-long **FloydWarshall(int N, std::vector<std::vector<int>> edges) {
-    long **distance = new long *[N + 1];
-    for (int i = 0; i <= N; i++) {
-        distance[i] = new long[N + 1];
-        for (int j = 0; j <= N; j++) {
-            if (i == j) {
-                distance[i][j] = 0;
-            } else {
-                distance[i][j] = 1e9;
-            }
-        }
-    }
-
-    for (int i = 0; i < edges.size(); i++) {
-        int start = edges[i][0];
-        int end = edges[i][1];
-        int weight = edges[i][2];
-        if (distance[start][end] > weight) {
-            distance[start][end] = weight;
-        }
-    }
-
-    for (int k = 1; k <= N; k++) {
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (distance[i][j] > distance[i][k] + distance[k][j]) {
-                    distance[i][j] = distance[i][k] + distance[k][j];
-                }
-            }
-        }
-    }
-
-    return distance;
-}
+#include "_11404.h"
 
 int main() {
     std::ios::sync_with_stdio(false);
@@ -51,7 +18,7 @@ int main() {
         edges.push_back({start, end, weight});
     }
 
-    long **result = FloydWarshall(N, edges);
+    long **result = _11404::FloydWarshall(N, edges);
 
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
---
+++ include/_11404.h
@@ -0,0 +1,14 @@
+//
+// Created by zero on 2024/02/12.
+//
+
+#ifndef BAEKJOON__11404_H
+#define BAEKJOON__11404_H
+
+#include <vector>
+
+namespace _11404 {
+    long **FloydWarshall(int N, std::vector<std::vector<int>> edges);
+}
+
+#endif //BAEKJOON__11404_H

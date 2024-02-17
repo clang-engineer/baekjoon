0e508f5d99929313298097def67ba3cbfd96c576
clang-engineer <clang.engineer@gmail.com>
2024-02-17 16:33:54 +0900

    faet: 11403 origin

--- app/main.cpp
+++ app/main.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
-#include <vector>
-
-#include "_11404.h"
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
     std::cout.tie(NULL);
 
-    int N, M;
-    std::cin >> N >> M;
+    int N;
+    long distances[101][101];
 
-    std::vector<std::vector<int>> edges;
-    for (int i = 0; i < M; i++) {
-        int start, end, weight;
-        std::cin >> start >> end >> weight;
-        edges.push_back({start, end, weight});
+    std::cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            std::cin >> distances[i][j];
+        }
     }
 
-    long **result = _11404::FloydWarshall(N, edges);
-
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-            if (result[i][j] == 1e9) {
-                std::cout << "0 ";
-            } else {
-                std::cout << result[i][j] << " ";
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (distances[i][k] == 1 && distances[k][j] == 1) {
+                    distances[i][j] = 1;
+                }
             }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            std::cout << distances[i][j] << " ";
         }
         std::cout << "\n";
     }

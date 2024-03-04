0e5ee7a94a9165e02d970eac7d7a0f300f64a1bf
clang-engineer <clang.engineer@gmail.com>
2024-03-04 22:06:15 +0900

    feat: 1991 origin

--- app/main.cpp
+++ app/main.cpp
@@ -1,36 +1,73 @@
 #include <iostream>
 
-#include  "_14425.h"
+void PreOrder(int tree[26][2], int now);
 
-using namespace _14425;
+void InOrder(int tree[26][2], int now);
+
+void PostOrder(int tree[26][2], int now);
 
 int main() {
     std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
 
-    int n, m;
-    std::cin >> n >> m;
-    Node *root = new Node();
+    int tree[26][2];
 
-    while (n > 0) {
-        char text[501];
-        std::cin >> text;
-        root->insert(text);
-        n--;
-    }
+    int n;
+    std::cin >> n;
 
-    int count = 0;
-    while (m > 0) {
-        char text[501];
-        std::cin >> text;
-        Node *node = root->find(text);
+    for (int i = 0; i < n; i++) {
+        char node_char, left, right;
+        std::cin >> node_char >> left >> right;
+        int node = node_char - 'A';
 
-        if (node != nullptr && node->isEnd) {
-            count++;
+        if (left == '.') {
+            tree[node][0] = -1;
+        } else {
+            tree[node][0] = left - 'A';
         }
-        m--;
+
+        if (right == '.') {
+            tree[node][1] = -1;
+        } else {
+            tree[node][1] = right - 'A';
+        }
     }
 
-    std::cout << count << '\n';
+    PreOrder(tree, 0);
+    std::cout << '\n';
+    InOrder(tree, 0);
+    std::cout << '\n';
+    PostOrder(tree, 0);
+}
+
+void PreOrder(int tree[26][2], int now) {
+    if (now == -1) {
+        return;
+    }
+
+    std::cout << (char) (now + 'A');
+
+    PreOrder(tree, tree[now][0]);
+    PreOrder(tree, tree[now][1]);
+}
+
+void InOrder(int tree[26][2], int now) {
+    if (now == -1) {
+        return;
+    }
+
+    InOrder(tree, tree[now][0]);
+    std::cout << (char) (now + 'A');
+    InOrder(tree, tree[now][1]);
+}
+
+void PostOrder(int tree[26][2], int now) {
+    if (now == -1) {
+        return;
+    }
+
+    PostOrder(tree, tree[now][0]);
+    PostOrder(tree, tree[now][1]);
+    std::cout << (char) (now + 'A');
 }

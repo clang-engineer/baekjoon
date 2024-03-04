75222fa8e71182deee1103a42da3fb551eb689ae
clang-engineer <clang.engineer@gmail.com>
2024-03-04 22:08:37 +0900

    feat: 1991 split header body

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
+#include "_1991.h"
 
-void PreOrder(int tree[26][2], int now);
-
-void InOrder(int tree[26][2], int now);
-
-void PostOrder(int tree[26][2], int now);
+using namespace _1991;
 
 int main() {
     std::ios::sync_with_stdio(false);
@@ -41,33 +38,3 @@ int main() {
     PostOrder(tree, 0);
 }
 
-void PreOrder(int tree[26][2], int now) {
-    if (now == -1) {
-        return;
-    }
-
-    std::cout << (char) (now + 'A');
-
-    PreOrder(tree, tree[now][0]);
-    PreOrder(tree, tree[now][1]);
-}
-
-void InOrder(int tree[26][2], int now) {
-    if (now == -1) {
-        return;
-    }
-
-    InOrder(tree, tree[now][0]);
-    std::cout << (char) (now + 'A');
-    InOrder(tree, tree[now][1]);
-}
-
-void PostOrder(int tree[26][2], int now) {
-    if (now == -1) {
-        return;
-    }
-
-    PostOrder(tree, tree[now][0]);
-    PostOrder(tree, tree[now][1]);
-    std::cout << (char) (now + 'A');
-}
---
+++ include/_1991.h
@@ -0,0 +1,16 @@
+//
+// Created by zero on 2024/03/04.
+//
+
+#ifndef BAEKJOON__1991_H
+#define BAEKJOON__1991_H
+
+namespace _1991 {
+    void PreOrder(int tree[26][2], int now);
+
+    void InOrder(int tree[26][2], int now);
+
+    void PostOrder(int tree[26][2], int now);
+}
+
+#endif //BAEKJOON__1991_H

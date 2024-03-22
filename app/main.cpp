f69d2e1f3027c370471fc298cb5182e738775d46
clang-engineer <clang.engineer@gmail.com>
2024-03-22 22:58:46 +0900

    feat: 10986 origin

--- app/main.cpp
+++ app/main.cpp
@@ -4,24 +4,37 @@
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
-    std::cin.tie(NULL);
+    std::cout.tie(NULL);
 
-    int N, M;
-    std::cin >> N >> M;
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<long> s(n, 0);
+    std::vector<long> c(m, 0);
+    long answer = 0;
 
-    std::vector<std::vector<int>> a(N + 1, std::vector<int>(N + 1, 0));
-    std::vector<std::vector<int>> d(N + 1, std::vector<int>(N + 1, 0));
+    std::cin >> s[0];
 
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-            std::cin >> a[i][j];
-            d[i][j] = d[i - 1][j] + d[i][j - 1] - d[i - 1][j - 1] + a[i][j];
+    for (int i = 1; i < n; i++) {
+        int temp;
+        std::cin >> temp;
+        s[i] = s[i - 1] + temp;
+    }
+
+    for (int i = 0; i < n; i++) {
+        int remainder = s[i] % m;
+
+        if (remainder == 0) {
+            answer++;
+        }
+
+        c[remainder]++;
+    }
+
+    for (int i = 0; i < m; i++) {
+        if (c[i] > 1) {
+            answer += c[i] * (c[i] - 1) / 2;
         }
     }
 
-    for (int i = 0; i < M; i++) {
-        int x1, y1, x2, y2;
-        std::cin >> x1 >> y1 >> x2 >> y2;
-        std::cout << d[x2][y2] - d[x1 - 1][y2] - d[x2][y1 - 1] + d[x1 - 1][y1 - 1] << '\n';
-    }
+    std::cout << answer << std::endl;
 }

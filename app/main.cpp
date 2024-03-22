119481a353203592c5f1955f3546a6f0fb3b0917
clang-engineer <clang.engineer@gmail.com>
2024-03-22 23:08:15 +0900

    feat: 2018 origin

--- app/main.cpp
+++ app/main.cpp
@@ -1,40 +1,31 @@
 #include <iostream>
-#include <vector>
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
     std::cout.tie(NULL);
 
-    int n, m;
-    std::cin >> n >> m;
-    std::vector<long> s(n, 0);
-    std::vector<long> c(m, 0);
-    long answer = 0;
+    int n;
+    std::cin >> n;
 
-    std::cin >> s[0];
+    int count = 1;
+    int start_index = 1;
+    int end_index = 1;
+    int sum = 1;
 
-    for (int i = 1; i < n; i++) {
-        int temp;
-        std::cin >> temp;
-        s[i] = s[i - 1] + temp;
-    }
-
-    for (int i = 0; i < n; i++) {
-        int remainder = s[i] % m;
-
-        if (remainder == 0) {
-            answer++;
-        }
-
-        c[remainder]++;
-    }
-
-    for (int i = 0; i < m; i++) {
-        if (c[i] > 1) {
-            answer += c[i] * (c[i] - 1) / 2;
+    while (end_index != n) {
+        if (sum == n){
+            count++;
+            end_index++;
+            sum += end_index;
+        } else if (sum > n) {
+            sum -= start_index;
+            start_index++;
+        } else {
+            end_index++;
+            sum += end_index;
         }
     }
 
-    std::cout << answer << std::endl;
+    std::cout << count << std::endl;
 }

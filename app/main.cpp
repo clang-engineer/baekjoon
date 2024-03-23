b529f21d832c362b8c0ace82e125d56cd580351a
clang-engineer <clang.engineer@gmail.com>
2024-03-23 12:43:04 +0900

    refactor: 17298 modularize

--- app/main.cpp
+++ app/main.cpp
@@ -1,27 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <stack>
 
-std::vector<int> GetNextGreaterElement(const std::vector<int> &arr) {
-    std::vector<int> answer(arr.size(), 0);
-    std::stack<int> stack;
-    stack.push(0);
-
-    for (int i = 1; i < arr.size(); i++) {
-        while (!stack.empty() && arr[stack.top()] < arr[i]) {
-            answer[stack.top()] = arr[i];
-            stack.pop();
-        }
-        stack.push(i);
-    }
-
-    while (!stack.empty()) {
-        answer[stack.top()] = -1;
-        stack.pop();
-    }
-
-    return answer;
-}
+#include "_17298.h"
 
 int main() {
     std::ios::sync_with_stdio(false);
@@ -36,7 +16,7 @@ int main() {
         std::cin >> arr[i];
     }
 
-    std::vector<int> answer = GetNextGreaterElement(arr);
+    std::vector<int> answer = _17298::GetNextGreaterElement(arr);
     for (int i = 0; i < n; i++) {
         std::cout << answer[i] << ' ';
     }

dfdfcd157a0cc682a59c55f501607c1e9c319142
clang-engineer <clang.engineer@gmail.com>
2024-03-23 12:37:42 +0900

    refactor: 17298 - split function

--- app/main.cpp
+++ app/main.cpp
@@ -2,24 +2,12 @@
 #include <vector>
 #include <stack>
 
-int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-
-    int n;
-    std::cin >> n;
-    std::vector<int> arr(n, 0);
-    std::vector<int> answer(n, 0);
-
-    for (int i = 0; i < n; i++) {
-        std::cin >> arr[i];
-    }
-
+std::vector<int> GetNextGreaterElement(const std::vector<int> &arr) {
+    std::vector<int> answer(arr.size(), 0);
     std::stack<int> stack;
     stack.push(0);
 
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < arr.size(); i++) {
         while (!stack.empty() && arr[stack.top()] < arr[i]) {
             answer[stack.top()] = arr[i];
             stack.pop();
@@ -32,6 +20,23 @@ int main() {
         stack.pop();
     }
 
+    return answer;
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int n;
+    std::cin >> n;
+    std::vector<int> arr(n, 0);
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+    }
+
+    std::vector<int> answer = GetNextGreaterElement(arr);
     for (int i = 0; i < n; i++) {
         std::cout << answer[i] << ' ';
     }

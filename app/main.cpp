c4f64723e2da51bc50de09095b684157688102ab
clang-engineer <clang.engineer@gmail.com>
2024-03-23 10:51:22 +0900

    feat: 12891 origin

--- app/main.cpp
+++ app/main.cpp
@@ -1,53 +1,108 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
-int CountPartialSummarized(const std::vector<int> &a);
+int check_arr[4];
+int my_arr[4];
+int check_secret = 0;
+
+void add(char c);
+
+void remove(char c);
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
 
-    int n;
-    std::cin >> n;
-    std::vector<int> a(n, 0);
+    int s, p;
+    std::cin >> s >> p;
 
-    for (int i = 0; i < n; ++i) {
-        std::cin >> a[i];
-    }
-    std::sort(a.begin(), a.end());
-
-    std::cout << CountPartialSummarized(a) << std::endl;
-}
-
-int CountPartialSummarized(const std::vector<int> &a) {
-    int n = a.size();
     int result = 0;
 
-    for (int i = 0; i < n; i++) {
-        long find = a[i];
-        int l = 0;
-        int r = n - 1;
+    std::string a;
+    std::cin >> a;
 
-        while (l < r) {
-            long sum = a[l] + a[r];
-            if (sum == find) {
-                if (l != i && r != i) {
-                    result++;
-                    break;
-                } else if (l == i) {
-                    l++;
-                } else if (r == i) {
-                    r--;
-                }
-            } else if (sum < find) {
-                l++;
-            } else {
-                r--;
-            }
+    for (int i = 0; i < 4; i++) {
+        std::cin >> check_arr[i];
+
+        if (check_arr[i] == 0) {
+            check_secret++;
         }
     }
 
-    return result;
+    for (int i = 0; i < p; i++) {
+        add(a[i]);
+    }
+
+    if (check_secret == 4) {
+        result++;
+    }
+
+    for (int i = p; i < s; i++) {
+        add(a[i]);
+        remove(a[i - p]);
+
+        if (check_secret == 4) {
+            result++;
+        }
+    }
+
+    std::cout << result << '\n';
+}
+
+void add(char c) {
+    switch (c) {
+        case 'A':
+            my_arr[0]++;
+            if (my_arr[0] == check_arr[0]) {
+                check_secret++;
+            }
+            break;
+        case 'C':
+            my_arr[1]++;
+            if (my_arr[1] == check_arr[1]) {
+                check_secret++;
+            }
+            break;
+        case 'G':
+            my_arr[2]++;
+            if (my_arr[2] == check_arr[2]) {
+                check_secret++;
+            }
+            break;
+        case 'T':
+            my_arr[3]++;
+            if (my_arr[3] == check_arr[3]) {
+                check_secret++;
+            }
+            break;
+    }
+}
+
+void remove(char c) {
+    switch (c) {
+        case 'A':
+            if (my_arr[0] == check_arr[0]) {
+                check_secret--;
+            }
+            my_arr[0]--;
+            break;
+        case 'C':
+            if (my_arr[1] == check_arr[1]) {
+                check_secret--;
+            }
+            my_arr[1]--;
+            break;
+        case 'G':
+            if (my_arr[2] == check_arr[2]) {
+                check_secret--;
+            }
+            my_arr[2]--;
+            break;
+        case 'T':
+            if (my_arr[3] == check_arr[3]) {
+                check_secret--;
+            }
+            my_arr[3]--;
+            break;
+    }
 }
